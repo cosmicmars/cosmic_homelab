cbd287934049f7a71346a7638552661ae350974d
elkaofficial <elkaofficial@elkaofficials-MacBook-Air.local>
2026-02-12 21:06:58 +0400

    bug fix

--- server.py
+++ server.py
@@ -19,9 +19,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_methods=["*"],
-    allow_headers=["*"],
+    aallow_origins = ["http://localhost:63342"],
+    allow_methods = ["*"],
+    allow_headers = ["*"],
 )
 
 client = None
