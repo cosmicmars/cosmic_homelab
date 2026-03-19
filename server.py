1a2e3684b29c09e74c55eec603de5157ca013f27
elkaofficial <elkaofficial@elkaofficials-MacBook-Air.local>
2026-03-19 19:50:00 +0400

    edit index.html

--- server.py
+++ server.py
@@ -300,4 +300,4 @@ async def stream_uptime(container_id: str, request: Request):
     )
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=1366)
